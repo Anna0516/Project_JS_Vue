268942c704b04668e10bc766be752c39bd20e2ff
Anna Eriksson <annaeriksson0516@gmail.com>
2022-01-30 21:16:03 +0100

    Edit space in component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,8 +7,8 @@
         <router-link to="/" class="nav-item nav-link">Hem</router-link>
         <router-link to="/about" class="nav-item nav-link">Om</router-link>
         <router-link to="/other/Det ena" class="nav-link">Det ena</router-link>
-        <router-link to="/other/Det andra" class="nav-link"
-          >Det andra</router-link
+        <router-link to="/other/Det andra" class="nav-link">
+          Det andra</router-link
         >
         <router-link to="/pink" class="nav-link">Rosa grejer</router-link>
       </div>
